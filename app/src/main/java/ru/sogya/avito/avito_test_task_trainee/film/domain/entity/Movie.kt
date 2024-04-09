88df901d9ed63e86e2ced529f86bedba89256d35
SogYa <Ghost.Spartan36@gmail.com>
2024-04-09 11:32:36 +0300

    renamed

--- app/src/main/java/ru/sogya/avito/avito_test_task_trainee/film/domain/entity/Movie.kt
+++ app/src/main/java/ru/sogya/avito/avito_test_task_trainee/film/domain/entity/Movie.kt
@@ -1,17 +1,21 @@
 package ru.sogya.avito.avito_test_task_trainee.film.domain.entity
 
-interface FilmDomain {
+interface Movie {
     val id: Int
     val name: String
     val alternativeName: String
-    val rating: Double
+    val rating: Rating
     val description: String
-    val genres: List<String>
-    val countries: List<String>
+    val genres: List<ResponseObject>
+    val countries: List<ResponseObject>
     val ageRating: Int
     val movieLength: Int
     val year: Int
     val type: String
     val poster: Poster
     val logo: Logo
+}
+
+interface ResponseObject{
+    val name: String
 }
