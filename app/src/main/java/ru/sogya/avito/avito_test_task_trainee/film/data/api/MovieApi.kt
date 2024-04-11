10e6295bd456be45998a2239e2c09554877947c0
SogYa <Ghost.Spartan36@gmail.com>
2024-04-11 18:58:40 +0300

    added movie api

---
+++ app/src/main/java/ru/sogya/avito/avito_test_task_trainee/film/data/api/MovieApi.kt
@@ -0,0 +1,28 @@
+package ru.sogya.avito.avito_test_task_trainee.film.data.api
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import ru.sogya.avito.avito_test_task_trainee.core.network.BaseResponse
+import ru.sogya.avito.avito_test_task_trainee.film.data.entity.FullMovieData
+import ru.sogya.avito.avito_test_task_trainee.film.data.entity.PosterData
+import ru.sogya.avito.avito_test_task_trainee.film.data.entity.ReviewData
+
+interface MovieApi {
+    @GET("movie/{movieId}")
+    suspend fun getMovieById(@Path("id") movieId: Int): FullMovieData
+
+    @GET("review")
+    suspend fun getReviewByMoviewId(
+        @Query("page") page: Int,
+        @Query("limit") pageSize: Int,
+        @Query("movieId") movieId: String
+    ): BaseResponse<ReviewData>
+
+    @GET("image")
+    suspend fun getPostersByMovieId(
+        @Query("page") page: Int,
+        @Query("limit") pageSize: Int,
+        @Query("movieId") movieId: String
+    ): BaseResponse<PosterData>
+}
