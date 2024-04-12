628a9a5bd5f1354b45600712d6806c5e164c3331
SogYa <Ghost.Spartan36@gmail.com>
2024-04-12 12:32:33 +0300

    deleted

--- app/src/main/java/ru/sogya/avito/avito_test_task_trainee/home/data/entity/MoviesResponse.kt
+++
@@ -1,9 +0,0 @@
-package ru.sogya.avito.avito_test_task_trainee.home.data.entity
-
-import com.google.gson.annotations.SerializedName
-import ru.sogya.avito.avito_test_task_trainee.film.data.entity.FilmData
-
-data class MoviesResponse(
-    @SerializedName("docs")
-    val docs: List<FilmData>
-)
