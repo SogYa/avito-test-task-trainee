9862be2e2fd8c8bb11073d8fd7906b27fb26ac9d
SogYa <Ghost.Spartan36@gmail.com>
2024-04-12 12:33:53 +0300

    added rating row

--- app/src/main/java/ru/sogya/avito/avito_test_task_trainee/core/uikit/component/RatingBox.kt
+++ app/src/main/java/ru/sogya/avito/avito_test_task_trainee/core/uikit/component/RatingBox.kt
@@ -2,6 +2,8 @@ package ru.sogya.avito.avito_test_task_trainee.core.uikit.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +28,29 @@ fun RatingBox(
         Text(
             modifier = Modifier.padding(horizontal = 2.dp),
             text = rating.toString()
+        )
+    }
+}
+
+@Composable
+fun RatingAndYearRow(
+    year: Int,
+    rating: Double
+) {
+    val color = when {
+        rating > 7 -> Color.Green
+        rating < 4 -> Color.Red
+        else -> Color.Gray
+    }
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            modifier = Modifier.padding(horizontal = 2.dp),
+            text = rating.toString(),
+            color = color
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = 2.dp),
+            text = year.toString(),
         )
     }
 }
