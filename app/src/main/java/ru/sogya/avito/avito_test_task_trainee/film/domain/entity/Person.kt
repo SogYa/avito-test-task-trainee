1a3c32797f80c17b20994ba2af7d9a00014563b1
SogYa <Ghost.Spartan36@gmail.com>
2024-04-12 18:29:01 +0300

    added description

--- app/src/main/java/ru/sogya/avito/avito_test_task_trainee/film/domain/entity/Person.kt
+++ app/src/main/java/ru/sogya/avito/avito_test_task_trainee/film/domain/entity/Person.kt
@@ -3,5 +3,6 @@ package ru.sogya.avito.avito_test_task_trainee.film.domain.entity
 interface Person {
     val photo: String
     val name: String
+    val description: String?
     val profession: String
 }
