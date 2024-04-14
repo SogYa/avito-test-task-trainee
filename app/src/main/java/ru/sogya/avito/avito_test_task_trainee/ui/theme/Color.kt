af3377d4a8099b6f1b3c4cd5d9b1178ab7b8dc5e
SogYa <Ghost.Spartan36@gmail.com>
2024-04-14 11:30:10 +0300

    added new colors

--- app/src/main/java/ru/sogya/avito/avito_test_task_trainee/ui/theme/Color.kt
+++ app/src/main/java/ru/sogya/avito/avito_test_task_trainee/ui/theme/Color.kt
@@ -6,10 +6,10 @@ internal val basePalette = TestAppColors(
     primary = Color(0xFF0F0F0F),
     secondary = Color(0xFF131315),
     tertiary = Color(0xFF1B1B1B),
-    accent = Color(0xFFEAFAB1),
+    accent = Color(0xFFE77D00),
     white = Color(0xFFFFFFFF),
     disable = Color(0xFF525252),
     secondaryText = Color(0xFF8C8C8C),
-    accentDisabled = Color(0x99EAFAB1),
+    accentDisabled = Color(0x99E77D00),
     unselectedWhite = Color.White
 )
