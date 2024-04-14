a8ddb05d0a618afc279dcb3339ca4bfea1a14c2d
SogYa <Ghost.Spartan36@gmail.com>
2024-04-14 11:30:30 +0300

    added base constructor

--- app/src/main/java/ru/sogya/avito/avito_test_task_trainee/home/data/entity/LogoData.kt
+++ app/src/main/java/ru/sogya/avito/avito_test_task_trainee/home/data/entity/LogoData.kt
@@ -2,4 +2,4 @@ package ru.sogya.avito.avito_test_task_trainee.home.data.entity
 
 import ru.sogya.avito.avito_test_task_trainee.home.domain.entity.Logo
 
-data class LogoData(override val url: String) : Logo
+data class LogoData(override val url: String = "") : Logo
--- app/src/main/java/ru/sogya/avito/avito_test_task_trainee/home/data/entity/MovieData.kt
+++ app/src/main/java/ru/sogya/avito/avito_test_task_trainee/home/data/entity/MovieData.kt
@@ -4,17 +4,17 @@ import ru.sogya.avito.avito_test_task_trainee.core.network.ResponseObject
 import ru.sogya.avito.avito_test_task_trainee.home.domain.entity.Movie
 
 data class MovieData(
-    override val id: Int,
-    override val name: String,
-    override val alternativeName: String,
-    override val rating: RatingData,
-    override val description: String,
-    override val genres: List<ResponseObject>,
-    override val countries: List<ResponseObject>,
-    override val ageRating: Int,
-    override val movieLength: Int,
-    override val year: Int,
-    override val type: String,
-    override val poster: PosterData,
-    override val logo: LogoData
+    override val id: Int = 0,
+    override val name: String = "",
+    override val alternativeName: String = "",
+    override val rating: RatingData = RatingData(),
+    override val description: String = "",
+    override val genres: List<ResponseObject> = listOf(),
+    override val countries: List<ResponseObject> = listOf(),
+    override val ageRating: Int = 0,
+    override val movieLength: Int = 0,
+    override val year: Int = 0,
+    override val type: String = "",
+    override val poster: PosterData = PosterData(),
+    override val logo: LogoData = LogoData()
 ) : Movie
